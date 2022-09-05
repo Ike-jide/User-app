e338da9c9a4d73cedb56e741452b9c6b41453ae2
Jideofor Ike <jideoforike@MacBook-Pro.local>
2022-09-05 19:31:09 +0100

    User-app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/UI/README.md
@@ -0,0 +1,18 @@
+# USER APP
+## _user-app_
+
+[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://nodesource.com/products/nsolid)
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+An app which deals in adding the information of diffrent users
+
+- Type some Markdown on the left
+- See HTML in the right
+- ✨Magic ✨
+
+## Features
+
+- Built using visual studio code
+- user-app built using react
+- resopnsive user app
